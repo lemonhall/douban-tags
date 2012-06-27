d50f716667b5c7521b481ea764df9b8619cc3228
lemonhall <lemonhall@gmail.com>
2012-06-27 23:49:06 +0800

    prefect ...sleep

--- contentscript.js
+++ contentscript.js
@@ -64,6 +64,9 @@ var	getUserName = function(){
 				return login_user;
 			}
 		},
+	//如果转到了自己给自己写邮件的页面则
+	//可以插入一个遮罩层，然后让用户察觉不到存储的过程，待搞定后再转回
+	//主界面，然后可以用spin.js来搞定AJAX效果什么的
 	redirecttoDouMail = function(){
 			var username=getUserName();
 				if(debug===1){console.log("username:"+username);}
@@ -91,81 +94,73 @@ var	getUserName = function(){
 						setTimeout(function(){
 								submit.click();
 						},5000);
+	},
+	initUpdateView = function (){
+			var doumail=$("a[href*='http://www.douban.com/doumail/']");
+			doumail.after("<a id='douban-tags-report'>收藏报告</a>");
+			var reshare_btn=$("div.actions a.btn-reshare");
+				reshare_btn.each(function(){
+						$(this).after("&nbsp;&nbsp;<a class='btn-tag-it'>收藏该条目</a>");
+				});
+	},
+	addFavBtn = function (){
+		//在Action条下运行的，收藏按钮
+		btn_tag_it=$("a.btn-tag-it");
+		btn_tag_it.bind("click",function(event){
+				redirecttoDouMail();
+				var myself=$(this).parent().parent().parent().parent();
+				//优先判断是否为值得存取的类型
+				//【存入数据库】类型
+				var data_kind=myself.attr("data-object-kind");
+				//【存入数据库】数据行为
+				var data_action=myself.attr("data-action");
+					if(debug==1){console.log("Action:"+data_action);}
+			//============================================
+				//打印人性化的提示信息
+				var action=datatypehash[data_kind]===undefined?data_kind:datatypehash[data_kind];
+					if(debug==1){console.log("Kind:"+action);}		
+				//【数据库KEY】SID
+				var data_sid=myself.attr("data-sid");
+					if(debug==1){console.log("ID:"+data_sid);}
+				//用户地址
+				var user_url=myself.find("div.bd p.text a:first").attr("href");
+					if(debug==1){console.log("user_url:"+user_url);}		
+				//用户的昵称
+				var user_name=myself.find("div.bd p.text a:first").html();
+					if(debug==1){console.log("user_name:"+user_name);}
+				//用户的发言
+				var user_quote=myself.find("div.bd blockquote p").html();
+					if(debug==1){console.log("user_quote:"+user_quote);}
+				//【存入数据库】用户的唯一ID
+				var user_uid=user_url.slice(29,-1);
+					if(debug==1){console.log("user_uid:"+user_uid);}
+				//【存入数据库】行为对象，div.bd p.text下的第二个a连接的href一般来说就是行为
+				var data_object=myself.find("div.bd p.text a:eq(1)").attr("href");
+					if(debug==1){console.log("行为对象:"+data_object);}
+				//【存入数据库】行为对象的描述
+				var data_description=myself.find("div.bd p.text a:eq(1)").html();
+					if(debug==1){console.log("行为对象:"+data_description);}
+				//【存入数据库？】时间对象？
+				var time=myself.find("div.actions span.created_at").attr("title");
+					if(debug==1){console.log("Time:"+time);}
+				//生成一个全局对象ID的URL并存入数据库
+				var uid_url=user_url+"status/"+data_sid;
+
+				});//End of 收藏 LocalStorage
+	},
+	router = function (){
+		if(urlParams["savebyme"]==="true"){
+				savetoDouMail();
+		}
+		if(location.href==="http://www.douban.com/doumail/"){
+
+		}
+		if(ifupdate_url){
+			initUpdateView();
+			addFavBtn();
+		}	
 	}
 
-//如果转到了自己给自己写邮件的页面则
-//可以插入一个遮罩层，然后让用户察觉不到存储的过程，待搞定后再转回
-//主界面，然后可以用spin.js来搞定AJAX效果什么的
-
-if(urlParams["savebyme"]==="true"){
-		savetoDouMail();
-
-}
-
-if(location.href==="http://www.douban.com/doumail/"){
-
-}
-
-//====================================================================
-//如果是在广播界面
-if(ifupdate_url){
-	//加入过滤器标示并建立好对象	
-	var doumail=$("a[href*='http://www.douban.com/doumail/']");
-
-//自动翻页功能相关代码段==================================================
-	doumail.after("<a id='douban-tags-report'>收藏报告</a>");
-
-//=========================================================================
-	
-//初始化的一些代码
-var reshare_btn=$("div.actions a.btn-reshare");
-	reshare_btn.each(function(){
-			$(this).after("&nbsp;&nbsp;<a class='btn-tag-it'>收藏该条目</a>");
-	});
-
-//在Action条下运行的，收藏按钮
-btn_tag_it=$("a.btn-tag-it");
-btn_tag_it.bind("click",function(event){
-	saveDatatoDouMail();
-var myself=$(this).parent().parent().parent().parent();
-	//优先判断是否为值得存取的类型
-	//【存入数据库】类型
-	var data_kind=myself.attr("data-object-kind");
-	//【存入数据库】数据行为
-	var data_action=myself.attr("data-action");
-		if(debug==1){console.log("Action:"+data_action);}
-//============================================
-	//打印人性化的提示信息
-	var action=datatypehash[data_kind]===undefined?data_kind:datatypehash[data_kind];
-		if(debug==1){console.log("Kind:"+action);}		
-	//【数据库KEY】SID
-	var data_sid=myself.attr("data-sid");
-		if(debug==1){console.log("ID:"+data_sid);}
-	//用户地址
-	var user_url=myself.find("div.bd p.text a:first").attr("href");
-		if(debug==1){console.log("user_url:"+user_url);}		
-	//用户的昵称
-	var user_name=myself.find("div.bd p.text a:first").html();
-		if(debug==1){console.log("user_name:"+user_name);}
-	//用户的发言
-	var user_quote=myself.find("div.bd blockquote p").html();
-		if(debug==1){console.log("user_quote:"+user_quote);}
-	//【存入数据库】用户的唯一ID
-	var user_uid=user_url.slice(29,-1);
-		if(debug==1){console.log("user_uid:"+user_uid);}
-	//【存入数据库】行为对象，div.bd p.text下的第二个a连接的href一般来说就是行为
-	var data_object=myself.find("div.bd p.text a:eq(1)").attr("href");
-		if(debug==1){console.log("行为对象:"+data_object);}
-	//【存入数据库】行为对象的描述
-	var data_description=myself.find("div.bd p.text a:eq(1)").html();
-		if(debug==1){console.log("行为对象:"+data_description);}
-	//【存入数据库？】时间对象？
-	var time=myself.find("div.actions span.created_at").attr("title");
-		if(debug==1){console.log("Time:"+time);}
-	//生成一个全局对象ID的URL并存入数据库
-	var uid_url=user_url+"status/"+data_sid;
-
-	});//End of 收藏 LocalStorage	
-}//End of if update view?
+	router();
  
 } )();
