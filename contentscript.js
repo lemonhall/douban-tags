0b5d1fd589ac582bf622fd995b78feffd085a64b
lemonhall <lemonhall@gmail.com>
2012-06-28 12:09:30 +0800

    prefect add local needtobeSynced counter and Sync logic more fast

--- contentscript.js
+++ contentscript.js
@@ -70,18 +70,50 @@ var	getUserName = function(){
 	redirecttoDouMail = function(){
 			var username=getUserName();
 				if(debug===1){console.log("username:"+username);}
-			location.href="http://www.douban.com/doumail/write?to="+username+"&savebyme=true";
+				setTimeout(function(){
+					location.href="http://www.douban.com/doumail/write?to="+username+"&savebyme=true";
+				},1000);
 
-	},stringify_database = function(){
+	},
+	clear_NeedbeSyncedCounter=function(){
+			localStorage["NeedbeSyncedCounter"]=0;
+	},
+	clear_NeedbeSyncedDataArray=function(){
+			var NeedbeSyncedDataArray=[];
+			localStorage["NeedbeSyncedDataArray"]=JSON.stringify(NeedbeSyncedDataArray);
+	},
+	getNeedbeSyncedDataArray=function(){
+		var NeedbeSyncedDataArray=[];
+		if(localStorage.hasOwnProperty("NeedbeSyncedDataArray")){
+			NeedbeSyncedDataArray=JSON.parse(localStorage["NeedbeSyncedDataArray"]);
+		}else{
+			//first blood
+			localStorage["NeedbeSyncedDataArray"]=JSON.stringify(NeedbeSyncedDataArray);
+		}
+		return NeedbeSyncedDataArray;
+	}
+	,stringify_database = function(){
 			var database=[];
 			var database_string="";
-				Object.keys(localStorage)
+			var NeedbeSyncedDataArray=[];
+			NeedbeSyncedDataArray=getNeedbeSyncedDataArray();
+				NeedbeSyncedDataArray
 			      .forEach(function(key){
 			          var status=JSON.parse(localStorage[key]);
-			              database.push(JSON.stringify(status));
+			          console.log(key);
+			          	if(status.hasSync == "true"){
+			          		//DO nothing
+			          	}else{
+			          		status.hasSync="true";
+			          		var newStatus=JSON.stringify(status);
+			          		localStorage[key]=newStatus;
+			              database.push(newStatus);
+			          	}
 			       });
 
 			    database_string=database.join(",");
+			    clear_NeedbeSyncedCounter();
+			    clear_NeedbeSyncedDataArray();
 			    return database_string;
 			    
 	},savetoDouMail= function(){
@@ -89,64 +121,144 @@ var	getUserName = function(){
 			var title=$("#mt");
 			var data=$("[name='m_text']");
 			var submit=$("[name='m_submit']");
-				title.val("database");				
-			data.val(stringify_database());
+				title.val("database");
+			var datatoSave=stringify_database();
+			data.val(datatoSave);
+
 						setTimeout(function(){
-								submit.click();
-						},5000);
+								//get this in timeout to wait for sync localstorage read and write
+									
+								//submit.click();
+						},1000);
+	},
+	getNeedbeSyncedCounter=function(){
+		var counter=0;
+		if(localStorage.hasOwnProperty("NeedbeSyncedCounter")){
+			counter=JSON.parse(localStorage["NeedbeSyncedCounter"]);
+		}else{
+			//first blood
+			localStorage["NeedbeSyncedCounter"]=counter;
+		}
+		return counter;
 	},
 	initUpdateView = function (){
 			var doumail=$("a[href*='http://www.douban.com/doumail/']");
-			doumail.after("<a id='douban-tags-report'>收藏报告</a>");
+
+			doumail.after("<a id='douban-tags-report'>收藏报告("+getNeedbeSyncedCounter()+")</a>");
 			var reshare_btn=$("div.actions a.btn-reshare");
 				reshare_btn.each(function(){
 						$(this).after("&nbsp;&nbsp;<a class='btn-tag-it'>收藏该条目</a>");
 				});
-	},
-	addFavBtn = function (){
-		//在Action条下运行的，收藏按钮
-		btn_tag_it=$("a.btn-tag-it");
-		btn_tag_it.bind("click",function(event){
+			var douban_tags_report=$("#douban-tags-report");
+			douban_tags_report.bind("click",function(event){
 				redirecttoDouMail();
-				var myself=$(this).parent().parent().parent().parent();
+			});			
+	},
+	getStatuData = function(objStatu){
 				//优先判断是否为值得存取的类型
 				//【存入数据库】类型
-				var data_kind=myself.attr("data-object-kind");
+				var data_kind=objStatu.attr("data-object-kind");
 				//【存入数据库】数据行为
-				var data_action=myself.attr("data-action");
+				var data_action=objStatu.attr("data-action");
 					if(debug==1){console.log("Action:"+data_action);}
 			//============================================
 				//打印人性化的提示信息
 				var action=datatypehash[data_kind]===undefined?data_kind:datatypehash[data_kind];
 					if(debug==1){console.log("Kind:"+action);}		
 				//【数据库KEY】SID
-				var data_sid=myself.attr("data-sid");
+				var data_sid=objStatu.attr("data-sid");
 					if(debug==1){console.log("ID:"+data_sid);}
 				//用户地址
-				var user_url=myself.find("div.bd p.text a:first").attr("href");
+				var user_url=objStatu.find("div.bd p.text a:first").attr("href");
 					if(debug==1){console.log("user_url:"+user_url);}		
 				//用户的昵称
-				var user_name=myself.find("div.bd p.text a:first").html();
+				var user_name=objStatu.find("div.bd p.text a:first").html();
 					if(debug==1){console.log("user_name:"+user_name);}
 				//用户的发言
-				var user_quote=myself.find("div.bd blockquote p").html();
+				var user_quote=objStatu.find("div.bd blockquote p").html();
 					if(debug==1){console.log("user_quote:"+user_quote);}
 				//【存入数据库】用户的唯一ID
 				var user_uid=user_url.slice(29,-1);
 					if(debug==1){console.log("user_uid:"+user_uid);}
 				//【存入数据库】行为对象，div.bd p.text下的第二个a连接的href一般来说就是行为
-				var data_object=myself.find("div.bd p.text a:eq(1)").attr("href");
+				var data_object=objStatu.find("div.bd p.text a:eq(1)").attr("href");
 					if(debug==1){console.log("行为对象:"+data_object);}
 				//【存入数据库】行为对象的描述
-				var data_description=myself.find("div.bd p.text a:eq(1)").html();
+				var data_description=objStatu.find("div.bd p.text a:eq(1)").html();
 					if(debug==1){console.log("行为对象:"+data_description);}
 				//【存入数据库？】时间对象？
-				var time=myself.find("div.actions span.created_at").attr("title");
+				var time=objStatu.find("div.actions span.created_at").attr("title");
 					if(debug==1){console.log("Time:"+time);}
 				//生成一个全局对象ID的URL并存入数据库
 				var uid_url=user_url+"status/"+data_sid;
 
-				});//End of 收藏 LocalStorage
+				var Statue={};
+					Statue.action=action;
+					Statue.data_sid=data_sid;
+					Statue.user_url=user_url;
+					Statue.user_name=user_name;
+					Statue.user_quote=user_quote;
+					Statue.user_uid=user_uid;
+					Statue.data_object=data_object;
+					Statue.data_description=data_description;
+					Statue.time=time;
+					Statue.uid_url=uid_url;
+
+				return Statue;
+
+	},
+	addNeedbeSyncedCounter=function(){
+		var counter=0;
+		if(localStorage.hasOwnProperty("NeedbeSyncedCounter")){
+			counter=JSON.parse(localStorage["NeedbeSyncedCounter"]);
+		}else{
+			//first blood
+			localStorage["NeedbeSyncedCounter"]=counter;
+		}
+
+		counter++;
+		localStorage["NeedbeSyncedCounter"]=counter;
+	},
+	addNeedbeSyncedDataID=function(NeedbeSyncedDataID){
+		var NeedbeSyncedDataArray=[];
+		if(localStorage.hasOwnProperty("NeedbeSyncedDataArray")){
+			NeedbeSyncedDataArray=JSON.parse(localStorage["NeedbeSyncedDataArray"]);
+		}else{
+			//first blood
+			localStorage["NeedbeSyncedDataArray"]=JSON.stringify(NeedbeSyncedDataArray);
+		}
+		NeedbeSyncedDataArray.push(NeedbeSyncedDataID);
+		localStorage["NeedbeSyncedDataArray"]=JSON.stringify(NeedbeSyncedDataArray);
+	},
+	savetoDB = function (_Statue){
+		if(localStorage.hasOwnProperty(_Statue.data_sid)){
+			_Statue=JSON.parse(localStorage[_Statue.data_sid]);
+		}else{
+			addNeedbeSyncedCounter();
+			addNeedbeSyncedDataID(_Statue.data_sid);
+		}
+		if (_Statue.hasSync=="true") {
+				//DO nothing
+		}else{
+			_Statue.hasSync="false";
+			
+			localStorage[_Statue.data_sid]=JSON.stringify(_Statue);
+		}		
+	},
+	addFavBtn = function (){
+		//在Action条下运行的，收藏按钮
+		btn_tag_it=$("a.btn-tag-it");
+
+		btn_tag_it.bind("click",function(event){
+				
+				var myself=$(this).parent().parent().parent().parent();
+				var oneStatue=getStatuData(myself);
+					if(debug==1){console.log("用户发言:"+oneStatue.user_quote);}
+				savetoDB(oneStatue);			
+				
+		});//End of 收藏 LocalStorage
+
+				
 	},
 	router = function (){
 		if(urlParams["savebyme"]==="true"){
